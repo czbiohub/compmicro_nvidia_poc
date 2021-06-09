17e3c918c221b4285b8556c0449dacb978ff945a
Bryant <bchhun@gmail.com>
2021-06-09 11:56:03 -0700

    stock resnet example

--- run_ddlrun_mpi.sh
+++ run_ddlrun_mpi.sh
@@ -6,7 +6,7 @@
 
 ddlrun --H cap \
 --accelerators 4 \
---mpiarg -pami_noib --oversubscribe \
+--mpiarg -pami_noib \
 python ./cli-ibm-ddlrun.py \
 -p /gpfs/CompMicro/projects/nVidia_POC/dynamorph/microglia \
 -o /gpfs/CompMicro/projects/nVidia_POC/dynamorph/microglia/JUNE/nvidia-poc-4GPU-ddlrun \
